0c38b6c1cf2e517ee491399b4bc70b8a7e137e1c
joshuacassidy <Joshcassidy79@gmail.com>
2018-01-29 20:09:39 +0000

    Fixed games API filename

--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,7 @@ var passport = require('passport');
 router.post('/', passport.authenticate('local'), (req, res, next) => {
   res.redirect('/')
 });
-
+ 
 router.get('/', (req, res) => {
   if (req.user) {
     res.redirect('/')
