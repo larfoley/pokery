aca5897f289e815216a30c6392bca1b3e77b73a2
joshuacassidy <Joshcassidy79@gmail.com>
2018-02-02 15:54:39 +0000

    Added the Location Schema

---
+++ models/Locations.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const pokerGameSchema = mongoose.Schema({
+	name:{
+		type: String,
+		required: true
+	 },
+	userId:{
+		type: String,
+		required: true
+	}
+});
