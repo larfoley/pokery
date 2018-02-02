00b42bd725c27348b8022210f31c35f832569182
joshuacassidy <Joshcassidy79@gmail.com>
2018-02-02 16:14:40 +0000

    Initial commit of locations file

---
+++ routes/locactions.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+
+
+router.get('/', (req, res) => {
+  
+})
+
+module.exports = router;
