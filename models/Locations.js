bcf5b6f1d38be27fa38669dbd7cb6de121cda6c5
joshuacassidy <Joshcassidy79@gmail.com>
2018-02-02 16:00:46 +0000

    formatted the code

--- models/Locations.js
+++ models/Locations.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
 
 const pokerGameSchema = mongoose.Schema({
-	name:{
-		type: String,
-		required: true
-	 },
-	userId:{
-		type: String,
-		required: true
-	}
+  name: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: String,
+    required: true
+  }
 });
 
 const GameName = module.exports = mongoose.model('games', pokerGameSchema);
 
-module.exports.getGameNames = (userId,callback) => GameName.find({userId},callback);
+module.exports.getGameNames = (userId, callback) => GameName.find({ userId }, callback);
 
 module.exports.getGameNameById = (id, callback) => GameName.findById(id, callback);
 
 module.exports.addGameName = (gameName, callback) => GameName.create(gameName, callback);
 
-module.exports.updateGameName = (id, name, callback) => GameName.findOneAndUpdate({_id: id}, {name}, {}, callback);
+module.exports.updateGameName = (id, name, callback) => GameName.findOneAndUpdate({ _id: id }, { name }, {}, callback);
 
-module.exports.removeGameName = (id, callback) => GameName.remove({_id: id}, callback);
+module.exports.removeGameName = (id, callback) => GameName.remove({ _id: id }, callback);
