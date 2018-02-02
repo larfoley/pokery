28ee6b53bac5ed4d6e96e4fe8234293a2f0976b3
joshuacassidy <Joshcassidy79@gmail.com>
2018-02-02 16:22:37 +0000

    added get request for the games

--- routes/locactions.js
+++ routes/locactions.js
@@ -3,10 +3,18 @@ const router = express.Router();
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+const GameName = require('../models/Locations');
+const keys = require('../keys');
+mongoose.connect(keys.db);
 
 
 router.get('/', (req, res) => {
-  
+  GameName.getGameNames(req.params.id, (err, gameNames) => {
+		if (err) {
+			throw err;
+		}
+		res.json(gameNames);
+	});
 })
 
 module.exports = router;
