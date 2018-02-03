580cebb81c6ed05c2b3e3f0e9a59ed3b21143774
joshuacassidy <Joshcassidy79@gmail.com>
2018-02-03 21:26:03 +0000

    added in neccessary files

--- routes/sesssion.js
+++ routes/sesssion.js
@@ -2,3 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const mongoose = require('mongoose');
+
+const GameName = require('../models/Locations');
+const keys = require('../keys');
+mongoose.connect(keys.db);
