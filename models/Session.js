4af737ea8e6ff7fd0dbeec27957df6b3be14423c
joshuacassidy <Joshcassidy79@gmail.com>
2018-02-03 21:18:57 +0000

    popualted session schema

--- models/Session.js
+++ models/Session.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
 const pokerSessionSchema = mongoose.Schema({
-
+  userId: {
+    type: String,
+		required: true
+  },
+  selectGame:{
+		type: String,
+		required: true
+  },
+  earnings:{
+		type: String,
+		required: true
+	 }
 });
