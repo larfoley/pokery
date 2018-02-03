e7e2d54740f675aea5bb591af0a268b3f64e41b7
joshuacassidy <Joshcassidy79@gmail.com>
2018-02-03 21:29:46 +0000

    added get all route for sessions

--- routes/sesssion.js
+++ routes/sesssion.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const passport = require('passport');
 const mongoose = require('mongoose');
 
-const GameName = require('../models/Locations');
+const Session = require('../models/Locations');
 const keys = require('../keys');
 mongoose.connect(keys.db);
+
+router.get('/api/session/:id', (req, res) => {
+	Session.getSessions(req.params.id, (err, session) => {
+		if (err) {
+			throw err;
+		}
+		res.json(session);
+	});
+});
