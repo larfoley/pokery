c7e7b96d7cbe6ff23e785828c21f523b54595546
larfoley <lar_x87@yahoo.com>
2018-02-11 20:24:49 +0000

    added form field component

---
+++ client/src/shared/FormField/Label.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Label = styled.label`
+  margin-bottom: 5px;
+  display: block;
+  color: #2e2e2e;
+  font-size: .9em;
+`
+
+export default Label
