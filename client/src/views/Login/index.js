876599c0f88784ef234240493b441a5ba771fd4e
EamonDoherty <eamondoherty1995@gmail.com>
2018-02-12 10:32:42 +0000

    added login view

---
+++ client/src/views/Login/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+import SectionTitle from "../../shared/SectionTitle"
+import PageSection from "../../shared/PageSection"
+import Login from "../../shared/Login/"
+
+const Home = props => {
+ 
+ 
+
+  return (
+    <div>
+
+    <PageSection>
+      <SectionTitle title="Login"/>
+      <Login />
+    </PageSection>
+
+  </div>
+  )
+
+}
+
+
+
+export default Home 
