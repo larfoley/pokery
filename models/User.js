b6c9fb9fc1fec43ebed017aaf804d084a4724a09
larfoley <lar_x87@yahoo.com>
2018-02-18 19:10:59 +0000

    refactored poker locations model

--- models/User.js
+++ models/User.js
@@ -1,34 +1,20 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
-var UserSchema = new mongoose.Schema({
-  username: {
-   type: String,
- },
- email: {
-   type: String,
- },
- hash: String,
-  // username: {
-  //   type: String,
-  //   required: true,
-  //   trim: true,
-  //   unique: true
-  // },
-  // email: {
-  //   type: String,
-  //   required: true,
-  //   trim: true,
-  // },
-  // hash: {
-  //   String,
-  //   trim: true,
-  // }
+var pokerLocationSchema = mongoose.Schema({
+  name: String
 });
+
+var UserSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  hash: String,
+  pokerLocations: [pokerLocationSchema]
+});
+
 
 UserSchema.methods.validPassword = (password, hash, callback) => {
   bcrypt.compare(password, hash, function(err, res) {
-    console.log("checking password");
     // callback(err, res)
     callback(err, password === hash)
   });
