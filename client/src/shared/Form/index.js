c0977e89a60b04fa927f3a3ffa5013dd4e4f4ac0
larfoley <lar_x87@yahoo.com>
2018-02-19 22:46:56 +0000

    added form component

---
+++ client/src/shared/Form/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Form = styled.form`
+  background-color: #2e2e2e;
+  max-width: 600px;
+  margin: auto;
+  padding: 3em 2em 2em 2em;
+  border-radius: 5px;
+`
+
+export default Form;
