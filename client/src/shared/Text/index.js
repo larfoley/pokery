dfe789721ce8c6f61ac30cce505fd31fcdcbb697
larfoley <lar_x87@yahoo.com>
2018-02-19 22:49:10 +0000

    added text component

---
+++ client/src/shared/Text/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Text = styled.p`
+  text-align: ${props => props.align};
+  color: ${props => props.color? props.color : null };
+  margin: ${props => props.margin? props.margin : null};
+  font-size: ${props => props.size? props.size : null};
+`
+
+export default Text
