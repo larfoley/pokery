46e2e683df034fd0f34db6824c19745f0cf4037e
yotsaphonSutweha <komsan_58@hotmail.com>
2018-02-20 19:20:18 +0000

    changed margin and line color

--- client/src/shared/LivePokerGame/Bar.js
+++ client/src/shared/LivePokerGame/Bar.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components'
 
 const Bar = styled.div`
-	border-bottom: 1px solid black;
+	border-bottom: 1px solid rgba(0, 0, 0, 0.1);
     & h3{
       display: block;
       font-size: 1.19 em;
       font-weight: bold;
-  }
+      margin-bottom: 0px;
+    }
+    & h4{
+      margin-top: 6px;
+    }
 `
 
 export default Bar
