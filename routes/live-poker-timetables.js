ab6fe84f268cf81501a3d09d98f1b86fa6aba8b2
joshuacassidy <Joshcassidy79@gmail.com>
2018-02-23 10:41:58 +0000

    readded the missing games back into the app

--- routes/live-poker-timetables.js
+++ routes/live-poker-timetables.js
@@ -9,25 +9,23 @@ router.get('/', (req, res) => {
 	const dayToFilterBy = req.query.day;
 
 	request('http://en.clubpoker.net/poker-tournaments', (error, response, html) => {
-
+		
 		const tournamentItemDate = itemDate =>
 			itemDate.children('.tournamentItemDate').children('.dateInfos');
-
 		const formatText = text =>
 			text.text().trim()
 
 		const tourneyTitle = title =>
 			title.children('.nextOccurrenceResult').children('.tournamentTitle')
-
+		
 		if (!error && response.statusCode == 200) {
 			const $ = cheerio.load(html);
 			const div = $('#ipsLayout_mainArea').children('.tournamentItem');
 			let id = 0;
-
+			
 			div.map((i, element) => {
 				const tournamentsGame = {};
 				tournamentsGame.id = id;
-
 				tournamentsGame.day = formatText($(element).children('.tournamentItemDate').children('.dateDay'))
 				tournamentsGame.prize = formatText($(element).children('.nextOccurrenceResult').children('div').children('.tournamentGaranteed'))
 
@@ -41,7 +39,7 @@ router.get('/', (req, res) => {
 					tournamentsGame.buyIn = x[0]
 
 					tournamentsGame.tournamentName = formatText(tourneyTitle($(element)))
-
+					console.log(tournamentsGame.tournamentName)
 					if ($(element).children('.nextOccurrenceResult').children('div').children('span').hasClass('tournamentGaranteed')) {
 						tournamentsGame.prize = formatText($(element).children('.nextOccurrenceResult').children('div').children('.tournamentGaranteed'))
 						tournamentsGame.type = formatText(tourneyTitle($(element)).next().next())
@@ -57,7 +55,7 @@ router.get('/', (req, res) => {
 					tournamentsGame.time = formatText(tournamentItemDate($(element)).children('.hour'))
 					let x = formatText($(element).children('.nextOccurrenceResult').children('.buyin'))
 					x = x.split(" ")
-					console.log(x)
+					// console.log(x)
 					tournamentsGame.currency = x[1]
 					tournamentsGame.buyIn = x[0]
 
@@ -66,9 +64,11 @@ router.get('/', (req, res) => {
 						tournamentsGame.type = formatText(tourneyTitle($(element)).next().next())
 						tournamentsGame.address = formatText(tourneyTitle($(element)).next().next())
 					} else {
-						tournamentsGame.type = formatText(tourneyTitle($(element).next()))
-						tournamentsGame.address = formatText(tourneyTitle($(element).next().next()))
+						tournamentsGame.type = formatText(tourneyTitle($(element).next().next()))
+						tournamentsGame.address = formatText(tourneyTitle($(element)).next().next())
 					}
+					tournamentsGames.push(tournamentsGame);
+					
 				} else {
 
 				}
