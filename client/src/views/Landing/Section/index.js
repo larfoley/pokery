7594f477901e1df2ad0b8c97eeef1f3a1b5071ea
larfoley <lar_x87@yahoo.com>
2018-03-04 18:01:31 +0000

    added landing section

---
+++ client/src/views/Landing/Section/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Section = styled.div`
+  padding: 4em 0;
+
+  @media screen and (min-width: 700px) {
+    padding: 6em 0;
+  }
+`
+
+export default Section
