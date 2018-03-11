b95b58c0938b81ac91166ea2398a1a81fede4563
larfoley <lar_x87@yahoo.com>
2018-03-11 19:21:52 +0000

    added combo box to filter games

--- client/src/views/FindAGame/index.js
+++ client/src/views/FindAGame/index.js
@@ -5,21 +5,30 @@ import LivePokerGames from "../../shared/LivePokerGames"
 import Header from '../../App/Header'
 import Footer from '../../App/Footer'
 import Sidebar from '../../App/Sidebar'
-import PageContainer from '../../App/PageContainer' 
+import PageContainer from '../../App/PageContainer'
 import SectionTitle from "../../shared/SectionTitle"
 import PageSection from "../../shared/PageSection"
+import ComboBox from "../../shared/ComboBox"
 
 const FindAGame = (props) => (
   <div>
-        <Header />
-        <Sidebar />
-        <PageContainer>
-            <PageSection>
-              <SectionTitle title="Find a Game"/>
-              <LivePokerGames limit={50} />
-            </PageSection>
-        </PageContainer>
-        <Footer />
-      </div>
+    <Header />
+    <Sidebar />
+    <PageContainer>
+        <PageSection>
+          <SectionTitle title="Find a Game"/>
+          <ComboBox width="150px" name="day" options={[
+            {value: "Today"},
+            {value: "Monday"},
+            {value: "Tuesday"},
+            {value: "Wednesday"},
+            {value: "Thursday"},
+            {value: "Friday"}
+          ]} />
+          <LivePokerGames limit={50} />
+        </PageSection>
+    </PageContainer>
+    <Footer />
+  </div>
 )
 export default FindAGame
