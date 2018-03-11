b882f86b848b0ad021df4f56800df06bc1ebdeb8
larfoley <lar_x87@yahoo.com>
2018-03-11 19:25:30 +0000

    added dummy poker sessions

--- client/src/views/SessionHistory/index.js
+++ client/src/views/SessionHistory/index.js
@@ -5,6 +5,7 @@ import Sidebar from '../../App/Sidebar'
 import PageContainer from '../../App/PageContainer'
 import SectionTitle from "../../shared/SectionTitle"
 import PageSection from "../../shared/PageSection"
+import PokerSessions from "../../shared/PokerSessions"
 
 const SessionsHistoryPage = props => {
   return (
@@ -13,9 +14,9 @@ const SessionsHistoryPage = props => {
       <Sidebar />
       <PageContainer>
         <PageSection>
-          <SectionTitle title="Session History"/>
+          <SectionTitle title="Sessions"/>
+          <PokerSessions />
         </PageSection>
-
       </PageContainer>
       <Footer />
     </div>
