bcea70894f723cb11c82f4c1afe6e6c382f4a5d7
larfoley <lar_x87@yahoo.com>
2018-03-11 19:31:41 +0000

    created component to store all poker sessions

---
+++ client/src/shared/PokerSessions/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PokerSession from './PokerSession'
+import axios from 'axios'
+
+class PokerSessions extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokerSessions: [
+        {earnings: 100},
+        {earnings: -20},
+        {earnings: -20}
+      ]
+    }
+  }
+
+  componentWillMount() {
+    axios.get('/api/poker-sessions')
+      .then(res => {
+        console.log(res);
+      })
+  }
+
+
+  render() {
+    return (
+      <div>
+        {this.state.pokerSessions.map(session => <PokerSession session={session} />)}
+      </div>
+    )
+  }
+
+}
+
+export default PokerSessions
