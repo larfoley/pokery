d421b4ef0b99dd8ecaa1561a95bb747e046103bf
joshuacassidy <Joshcassidy79@gmail.com>
2018-03-13 22:50:57 +0000

    Updated the chart data and win data

--- client/src/views/Progress/index.js
+++ client/src/views/Progress/index.js
@@ -22,13 +22,13 @@ import {
 } from 'recharts'
 
 const data = [
-  {name: 'Jan', uv: 4000, pv: 2400, amt: 2400},
-  {name: 'Feb', uv: 3000, pv: 1398, amt: 2210},
-  {name: 'March', uv: 2000, pv: 9800, amt: 2290},
-  {name: 'April', uv: 2780, pv: 3908, amt: 2000},
-  {name: 'May', uv: 1890, pv: 4800, amt: 2181},
-  {name: 'June', uv: 2390, pv: 3800, amt: 2500},
-  {name: 'July', uv: 3490, pv: 4300, amt: 2100},
+  {name: 'Jan', uv: 400, pv: 240, amt: 200},
+  {name: 'Feb', uv: 300, pv: 138, amt: 221},
+  {name: 'March', uv: 200, pv: 680, amt: 229},
+  {name: 'April', uv: 278, pv: 308, amt: 200},
+  {name: 'May', uv: 189, pv: 400, amt: 218},
+  {name: 'June', uv: 239, pv: 300, amt: 250},
+  {name: 'July', uv: 349, pv: 430, amt: 210},
 ]
 
 const dataBar = [
@@ -83,7 +83,7 @@ const Home = props => {
         </PageSection>
 
         <PageSection>
-          <SectionTitle title="Life Time Earnings"/>
+          <SectionTitle title="Life Time Earnings By Wins"/>
             <AreaChart width={730} height={250} data={data}
               margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
               <defs>
