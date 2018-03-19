9469c1ed2584648c216d0208012bf4a083c3de94
joshuacassidy <Joshcassidy79@gmail.com>
2018-03-19 21:00:15 +0000

    removed unneccessry data

--- client/src/views/Progress/PieCharts/index.js
+++ client/src/views/Progress/PieCharts/index.js
@@ -19,25 +19,6 @@ import {
   Label
 } from 'recharts'
 
-const data = [
-  {name: 'Jan', Cash: 400, Tournament: 240, amt: 200},
-  {name: 'Feb', Cash: 300, Tournament: 138, amt: 221},
-  {name: 'March', Cash: 200, Tournament: 680, amt: 229},
-  {name: 'April', Cash: 278, Tournament: 308, amt: 200},
-  {name: 'May', Cash: 189, Tournament: 400, amt: 218},
-  {name: 'June', Cash: 239, Tournament: 300, amt: 250},
-  {name: 'July', Cash: 349, Tournament: 430, amt: 210},
-]
-
-const dataBar = [
-  {name: 'Village Green', Cash: 10, Tournaments: 19, amt: 100},
-  {name: 'Fitzwilliam Casino', Cash: 15, Tournaments: 7, amt: 100},
-  {name: 'Macau Casino', Cash: 2, Tournaments: 9, amt: 100},
-  // {name: 'Eglinton', Cash: 6, Tournaments: 3, amt: 100},
-  // {name: 'Sporting Emporium', Cash: 8, Tournaments: 9, amt: 100},
-  // {name: 'Penthouse Casino', Cash: 1, Tournaments: 11, amt: 100},
-  {name: 'Green Room', Cash: 17, Tournaments: 2, amt: 100},
-]
 
 const data01 = [
   {name: 'Green Room', value: 29},
