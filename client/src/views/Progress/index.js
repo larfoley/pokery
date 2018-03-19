f307b9f2f77a0476fc8bb3239ab8e5e99463376f
joshuacassidy <Joshcassidy79@gmail.com>
2018-03-19 15:19:46 +0000

    added label on the y axis for the line chart

--- client/src/views/Progress/index.js
+++ client/src/views/Progress/index.js
@@ -129,7 +129,7 @@ const Home = props => {
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
-          <YAxis  />
+          <YAxis label={{ value: '€', position: 'insideTopLeft' }} />
           <Tooltip />
           <Legend />
           <Line type="monotone" dataKey="Cash" stroke="#8884d8" />
