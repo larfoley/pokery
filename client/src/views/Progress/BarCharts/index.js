ae3a801d123c3487a2a44751f859075e8b13891d
joshuacassidy <Joshcassidy79@gmail.com>
2018-03-19 21:33:09 +0000

    removed unneccary dependancies

--- client/src/views/Progress/BarCharts/index.js
+++ client/src/views/Progress/BarCharts/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import SectionTitle from "../../../shared/SectionTitle"
 import PageSection from "../../../shared/PageSection"
-import PageContainer from '../../../App/PageContainer'
 import {
   CartesianGrid,
   XAxis,
@@ -14,22 +13,20 @@ import {
 
 const BarCharts = props => {
   return (
-        <PageSection>
-          <SectionTitle title={props.title}/>
-          <BarChart width={props.width} height={props.height} data={props.data}>
-            <CartesianGrid strokeDasharray={props.strokeDasharray} />
-            <XAxis dataKey={props.xDataKey} />
-            <YAxis label={props.label} />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey={props.dataKey1} fill={props.fill1} />
-            <Bar dataKey={props.dataKey2} fill={props.fill2} />
-          </BarChart>
-        </PageSection>
+    <PageSection>
+      <SectionTitle title={props.title}/>
+      <BarChart width={props.width} height={props.height} data={props.data}>
+        <CartesianGrid strokeDasharray={props.strokeDasharray} />
+        <XAxis dataKey={props.xDataKey} />
+        <YAxis label={props.label} />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey={props.dataKey1} fill={props.fill1} />
+        <Bar dataKey={props.dataKey2} fill={props.fill2} />
+      </BarChart>
+    </PageSection>
   )
 
 }
-
-
 
 export default BarCharts
