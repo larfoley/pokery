818b5c93ea35f46c7ee3cf93a88128078776e80d
joshuacassidy <Joshcassidy79@gmail.com>
2018-03-19 22:05:30 +0000

    removed unused data

--- client/src/views/Progress/AreaChart/AreaCharts.js
+++ client/src/views/Progress/AreaChart/AreaCharts.js
@@ -38,52 +38,33 @@ const data = [
   {name: 'July', Cash: 349, Tournament: 430, amt: 210},
 ]
 
-const dataBar = [
-  {name: 'Village Green', Cash: 10, Tournaments: 19, amt: 100},
-  {name: 'Fitzwilliam Casino', Cash: 15, Tournaments: 7, amt: 100},
-  {name: 'Macau Casino', Cash: 2, Tournaments: 9, amt: 100},
-  // {name: 'Eglinton', Cash: 6, Tournaments: 3, amt: 100},
-  // {name: 'Sporting Emporium', Cash: 8, Tournaments: 9, amt: 100},
-  // {name: 'Penthouse Casino', Cash: 1, Tournaments: 11, amt: 100},
-  {name: 'Green Room', Cash: 17, Tournaments: 2, amt: 100},
-]
-
-const data01 = [
-  {name: 'Green Room', value: 29},
-  {name: 'Village Green', value: 22},
-  {name: 'Fitzwilliam Casino', value: 11},
-  {name: 'Green Room', value: 19}
-]
-
-const Home = props => {
+const AreaCharts = props => {
   return (
-        <PageSection>
-          <SectionTitle title="Life Time Earnings"/>
-            <AreaChart width={730} height={250} data={data}
-              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-              <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                  <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                </linearGradient>
-                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-                </linearGradient>
-              </defs>
-              <XAxis dataKey="name" />
-              <YAxis label={{ value: '€', position: 'insideTopLeft' }} />
-              <CartesianGrid strokeDasharray="3 3" />
-              <Tooltip />
-              <Legend />
-             <Area type="monotone" dataKey="Cash" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-              <Area type="monotone" dataKey="Tournament" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
-            </AreaChart>
-        </PageSection>
+    <PageSection>
+      <SectionTitle title="Life Time Earnings"/>
+        <AreaChart width={730} height={250} data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis label={{ value: '€', position: 'insideTopLeft' }} />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Area type="monotone" dataKey="Cash" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+          <Area type="monotone" dataKey="Tournament" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+        </AreaChart>
+    </PageSection>
   )
 
 }
 
-
-
-export default Home
+export default AreaCharts
