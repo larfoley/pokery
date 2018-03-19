23d8351bb8c53ba69e9ea0928f88979ecf560d63
joshuacassidy <Joshcassidy79@gmail.com>
2018-03-19 22:40:08 +0000

    removed sample data in child component

--- client/src/views/Progress/AreaCharts/index.js
+++ client/src/views/Progress/AreaCharts/index.js
@@ -12,16 +12,6 @@ import {
   Legend,
 } from 'recharts'
 
-const data = [
-  {name: 'Jan', Cash: 400, Tournament: 240, amt: 200},
-  {name: 'Feb', Cash: 300, Tournament: 138, amt: 221},
-  {name: 'March', Cash: 200, Tournament: 680, amt: 229},
-  {name: 'April', Cash: 278, Tournament: 308, amt: 200},
-  {name: 'May', Cash: 189, Tournament: 400, amt: 218},
-  {name: 'June', Cash: 239, Tournament: 300, amt: 250},
-  {name: 'July', Cash: 349, Tournament: 430, amt: 210},
-]
-
 const AreaCharts = props => {
   return (
     <PageSection>
