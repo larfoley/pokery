b7db6d216468a6e5169b5afeb0c2753af13cb80f
EamonDoherty <eamondoherty1995@gmail.com>
2018-03-21 18:55:46 +0000

    Update index.js
    Updated landing page text

--- client/src/views/Landing/Features/index.js
+++ client/src/views/Landing/Features/index.js
@@ -11,19 +11,19 @@ const Features = props => (
           <Feature
             icon="line-chart"
             title="Track Your Progress"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore qui ratione ducimus eius maxime obcaecati totam culpa voluptatibus odit! Esse veniam soluta atque tenetur animi tempora! Rem mollitia ipsam doloremque?"/>
+            description="A core feature Pokery has for users to utilise, is to track your progress over time. This feature is for any poker player who has a desire or willingness to  enhance their playing skills and odds in the near future"/>
         </div>
         <div className="grid__col grid__col--2-of-6">
           <Feature
             icon="search"
             title="Find Live Poker"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore qui ratione ducimus eius maxime obcaecati totam culpa voluptatibus odit! Esse veniam soluta atque tenetur animi tempora! Rem mollitia ipsam doloremque?"/>
+            description="A massive tool for poker players is to find a nearby poker game or tournament, whether its a pub, poker establishment or another Pokery users property a player can find what is nearby to further improve their skills"/>
         </div>
         <div className="grid__col grid__col--2-of-6">
           <Feature
             icon="plus"
             title="Personalized Statistics"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore qui ratione ducimus eius maxime obcaecati totam culpa voluptatibus odit! Esse veniam soluta atque tenetur animi tempora! Rem mollitia ipsam doloremque?"/>
+            description="An immense feature found while using Pokery is the personalized statistics and graphs we will give to the user. The idea is to help all Pokery users learn what they are best at and how they can improve in the future"/>
         </div>
       </div>
     </Section>
