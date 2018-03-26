53ead5c4ffcad3fa008466b3d1da9a575b764275
larfoley <lar_x87@yahoo.com>
2018-03-26 11:46:02 +0100

    refactored Input component

---
+++ client/src/shared/Input/Label.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const Label = styled.label`
+  margin-bottom: 8px;
+  display: block;
+`
+
+export default Label
--- client/src/shared/Input/index.js
+++ client/src/shared/Input/index.js
@@ -1,11 +1,34 @@
+import React from 'react'
 import styled from 'styled-components'
+import Label from './Label'
 
 const StyledInput = styled.input`
-   border: 1px solid #eee;
-   padding: .5em;
-   margin-bottom: 1em;
-   display: block;
-   width: 100%;
-`;
+  ${'' /* border: none;
+  color: white;
+  font-family: inherit;
+  display: block;
+  width: 100%;
+  margin-bottom: 1.7em;
+  padding: 1.5em;
+  border-radius: 4px; */}
 
-export default StyledInput
+  border: 1px solid #eee;
+  padding: 1em;
+  margin-bottom: 1em;
+  display: block;
+  width: 100%;
+`
+
+const Input = props => (
+  <div>
+    {props.label? <Label htmlFor="" >{props.label}</Label> : null}
+    <StyledInput
+      type={props.type}
+      placeholder={props.placeholder}
+      onChange={props.onChange}
+      name={props.name}
+    />
+  </div>
+)
+
+export default Input
