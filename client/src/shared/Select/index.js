b6f8cd704be24699cba529cf4a6c846531532190
larfoley <lar_x87@yahoo.com>
2018-03-26 11:47:21 +0100

    Renamed and Refactored ComboBox Component to Select

---
+++ client/src/shared/Select/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+import Label from './Label'
+
+const StyledSelect = styled.select`
+  border: none;
+  background-color: white;
+  border: 1px solid #eee;
+  padding: 1em;
+  width: 100%;
+  font-family: inherit;
+  margin-bottom: 1em;
+  font-size: 1em;
+  border-radius: 4px;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+`
+
+
+const Select = props => (
+  <div>
+    <Label>{props.label}</Label>
+    <StyledSelect
+      border={props.border}
+      onChange={props.onChange}
+      name={props.name}
+      >
+      {
+        props.options? props.options.map(option => (
+          <option key={option}>{option}</option>
+        ))
+        : null
+      }
+    </StyledSelect>
+  </div>
+)
+
+export default Select
