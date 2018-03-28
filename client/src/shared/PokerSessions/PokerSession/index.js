a45acb6258c4470a8c65e54a102c7e8e4bdccd60
yotsaphonSutweha <komsan_58@hotmail.com>
2018-03-28 19:35:11 +0100

    put up initial component looks

--- client/src/shared/PokerSessions/PokerSession/index.js
+++ client/src/shared/PokerSessions/PokerSession/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import Box from '../../Box'
-
+import Table from '../../LivePokerGames/LivePokerGame/Table'
+import TableBox from '../../LivePokerGames/LivePokerGame/TableBox'
+import Wrapper from '../../LivePokerGames/LivePokerGame/Wrapper'
+import Header from '../../LivePokerGames/LivePokerGame/Header'
 const PokerSesion = props => (
-  <Box>
-    Earnings: {props.session.earnings}
-  </Box>
+ <Wrapper>
+   <Header>
+     <h3>Hi</h3>
+   </Header>
+   {/* <Box>
+     Earnings: {props.session.earnings}
+   </Box> */}
+   <TableBox>
+    <Table>
+      <table>
+        <tbody>
+         <tr>
+          <th>Earnings:</th>
+          <td>{props.session.earnings}</td>
+         </tr>
+        </tbody>
+      </table>
+    </Table>
+   </TableBox>
+  </Wrapper>
 )
 
 export default PokerSesion
