4a6f961080c7adf2492db05bb41115100a2df958
joshuacassidy <Joshcassidy79@gmail.com>
2018-03-29 11:17:07 +0100

    removed get request in change email route

--- routes/change-email.js
+++ routes/change-email.js
@@ -5,24 +5,13 @@ const bcrypt = require('bcrypt')
 router.post('/', (req, res) => {
   const _id = req.body._id;
   const email = req.body.email;
-
   User.findById(_id, (err, user) => {
       User.findOneAndUpdate({_id}, {email}, {}, (err, user) => {
         if(err) {
           throw err
         }
-        console.log(user)
         res.json({user})
       });
-  });
-})
-
-router.get('/', (req, res) => {
-  User.find((err, lists) => {
-		if(err){
-			throw err;
-		}
-		res.json(lists);
   });
 })
 
