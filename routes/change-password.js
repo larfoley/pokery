bd4385b07d807de71f169e0dad40809d0601d384
joshuacassidy <Joshcassidy79@gmail.com>
2018-03-29 11:04:57 +0100

    cleaned up code more

--- routes/change-password.js
+++ routes/change-password.js
@@ -3,23 +3,21 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt')
 
 router.post('/', (req, res) => {
-  const _id = req.body.id;
-  const password = req.body.newPassword;
-  
+  const _id = req.body._id;
+  const password = req.body.password;
+
   User.findById(_id, (err, user) => {
     bcrypt.hash(password, 10, (err, hash) => {
-      if (err) return next(err)
-      
-      User.findOneAndUpdate({_id: _id}, {hash: hash}, {}, (err, user) => {
-        if(err){
-          throw err;
+      if (err) {
+        return next(err)
+      }
+      User.findOneAndUpdate({_id}, {hash}, {}, (err, user) => {
+        if(err) {
+          throw err
         }
-        console.log("\nnew user" + user)
+        res.json({user})
       });
     });
-
-    res.json({"1":1})
-    
   });
 })
 
