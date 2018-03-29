67ccb77f086cacc06959caa24832c107d13d442f
joshuacassidy <Joshcassidy79@gmail.com>
2018-03-29 10:47:00 +0100

    added mongo and bcrypt

--- routes/change-password.js
+++ routes/change-password.js
@@ -1,7 +1,30 @@
 var router = require("express").Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt')
 
 router.post('/', (req, res) => {
-  res.json({"1":1})
+  console.log(req.body)
+  User.findById(req.body.id, (err, user) => {
+    console.log(user)
+
+    bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
+      if (err) return next(err)
+      console.log("hash: " + hash)      
+    });
+
+    res.json({"1":1})
+    
+  });
+})
+
+router.get('/', (req, res) => {
+  User.find((err, lists) => {
+		if(err){
+      console.log(err)
+			throw err;
+		}
+		res.json(lists);
+  });
 })
 
 module.exports = router
