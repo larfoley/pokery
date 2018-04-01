128f65e08fe085595f02af8dfe19496edb1555f2
larfoley <lar_x87@yahoo.com>
2018-04-01 12:02:01 +0100

    refactored change-password route

--- routes/change-password.js
+++ routes/change-password.js
@@ -3,23 +3,21 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt')
 const requiresAuth = require('../middleware/requiresAuth.js')
 
-router.post('/', requiresAuth, (req, res) => {
-  const _id = req.body._id;
-  const password = req.body.password;
+router.post('/', requiresAuth, (req, res, next) => {
 
-  User.findById(_id, (err, user) => {
-    bcrypt.hash(password, 10, (err, hash) => {
-      if (err) {
-        return next(err)
-      }
-      User.findOneAndUpdate({_id}, {hash}, {}, (err, user) => {
-        if(err) {
-          throw err
-        }
-        res.json({user})
-      });
-    });
+  const password = req.body.password
+  const username = req.user.username
+
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) return next(err)
+
+    User.findOneAndUpdate({ username }, { hash }, (err, doc) => {
+      if (err) next(err)
+      res.json({message: "password updated"})
+    })
+
   });
+
 })
 
 module.exports = router
