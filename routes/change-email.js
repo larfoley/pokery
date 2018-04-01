e7f8cca17f5c7d04416c54c8657791f2209a988f
larfoley <lar_x87@yahoo.com>
2018-04-01 12:02:19 +0100

    refactored change-email route

--- routes/change-email.js
+++ routes/change-email.js
@@ -1,19 +1,16 @@
 var router = require("express").Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt')
 const requiresAuth = require('../middleware/requiresAuth.js')
 
-router.post('/', requiresAuth, (req, res) => {
-  const _id = req.body._id;
-  const email = req.body.email;
-  User.findById(_id, (err, user) => {
-      User.findOneAndUpdate({_id}, {email}, {}, (err, user) => {
-        if(err) {
-          throw err
-        }
-        res.json({user})
-      });
-  });
+router.post('/', requiresAuth, (req, res, next) => {
+  const email = req.body.email
+  const username = req.user.username
+
+  User.findOneAndUpdate({ username }, { email }, (err, doc) => {
+    if (err) next(err)
+    res.json({message: "email updated"})
+  })
+
 })
 
 module.exports = router
