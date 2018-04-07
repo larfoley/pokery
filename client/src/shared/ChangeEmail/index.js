c2b2a17dfbf043f0414d1bba9c92ac93f8ff347f
larfoley <lar_x87@yahoo.com>
2018-04-07 19:42:42 +0100

    updated labels

--- client/src/shared/ChangeEmail/index.js
+++ client/src/shared/ChangeEmail/index.js
@@ -4,24 +4,21 @@ import Select from '../Select'
 import Input from '../Input'
 
 class ChangeEmail extends React.Component {
-  	constructor() {
-		super()
-		this.state = {
-			email: '',
-			emailCheck: '',
-		};
-		this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleChange = this.handleChange.bind(this)
-	} 	
+
+  constructor() {
+  	super()
+  	this.state = {
+  		email: '',
+  	};
+  	this.handleSubmit = this.handleSubmit.bind(this)
+  	this.handleChange = this.handleChange.bind(this)
+	}
+
 	handleSubmit(event) {
 		event.preventDefault()
-		if ((this.state.email.length > 6) && (this.state.email==this.state.emailCheck)){
-			console.log(this.state);
-			window.alert("New email submitted")
-		} else {
-			window.alert("New email mismatch")
-		}
-	} 
+    window.alert("Email Updated")
+	}
+
 	handleChange(event) {
 		const name = event.target.name
 		const value = event.target.value
@@ -30,29 +27,21 @@ class ChangeEmail extends React.Component {
 			return prevState
 		})
 	}
-	
+
 	render() {
-		return (	
+		return (
 			<form onSubmit={this.handleSubmit}>
-			
 				<Input
+          type="email"
 					name="email"
 					value={this.state.email}
-					label="New Email"          			
+					label="New Email"
 					onChange={this.handleChange}
 				/>
-				<Input
-					name="emailCheck"
-					value={this.state.emailCheck}
-					label="Retype New Email"
-					onChange={this.handleChange}
-				/>
-				<Button type="submit">Update</Button>
+				<Button type="submit">Update Email</Button>
 			</form>
 		)
-  	}
+  }
 }
 
 export default ChangeEmail
-
-
