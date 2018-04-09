933199a2bf778f3d076085b46184038d7e643bf5
larfoley <lar_x87@yahoo.com>
2018-04-09 20:50:46 +0100

    fixed BestLocationsChart

--- client/src/views/Progress/BestLocationsChart/index.js
+++ client/src/views/Progress/BestLocationsChart/index.js
@@ -7,21 +7,51 @@ import {
 
 const BestLocationsChart = props => {
 
-  // const data = props.sessions.map(session => ({
-  //   name: session.name, value: 29
-  // }))
+  const mostSuccesfullLocations = []
+  const locations = []
+  const sessionsFilteredByLocation = {}
 
-  const data01 = [
-    {name: 'Green Room', value: 29},
-    {name: 'Village Green', value: 22},
-    {name: 'Fitzwilliam Casino', value: 11},
-    {name: 'Green Room', value: 19}
-  ]
+  // get all locations
+  props.sessions.map(session => {
+    let locationExists = false
+    let location;
+
+    locations.forEach(l => {
+      if (session.location === l) {
+        locationExists = true
+      }
+    })
+
+    if (!locationExists) {
+      locations.push(session.location)
+    }
+  })
+
+  // filter sessions by location
+  locations.forEach(location => {
+    sessionsFilteredByLocation[location] = props.sessions
+      .filter(session => session.location === location)
+  })
+
+  // Calclate total earnings of each location
+  for (var key in sessionsFilteredByLocation) {
+    let totalEarnings = 0
+
+    sessionsFilteredByLocation[key].forEach(session => {
+      totalEarnings += parseInt(session.amountWon - session.buyIn)
+    })
+
+    mostSuccesfullLocations.push({
+      name: key,
+      value: totalEarnings
+    })
+
+  }
 
   return (
     <PieChart width={730} height={250}>
       <Pie
-        data={data01}
+        data={mostSuccesfullLocations}
         dataKey="value"
         nameKey="name"
         cx="50%"
