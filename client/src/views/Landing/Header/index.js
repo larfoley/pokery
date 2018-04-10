68b9cb95bac5796b1df95a767db62b1eecb654d6
larfoley <lar_x87@yahoo.com>
2018-04-10 20:13:32 +0100

    updated landing page

--- client/src/views/Landing/Header/index.js
+++ client/src/views/Landing/Header/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import Button from '../../../shared/Button'
+import ButtonLink from '../../../shared/ButtonLink'
 import Navbar from '../Navbar'
 import Container from '../Container'
 import Section from '../Section'
 
 const Wrapper = styled.div`
-  background-color: #2e2e2e;
-  color: white;
+  background-color:  #4c505d;
+  ${'' /* color: white; */}
+  text-align: center;
   ${'' /* padding: 4em 0; */}
 
   @media screen and (min-width: 700px) {
@@ -17,11 +18,25 @@ const Wrapper = styled.div`
 
 const Title = styled.h1`
   margin: 0;
+  font-size: 2.5em;
+  color: white;
+
+  &::after {
+    content: " ";
+    display: block;
+    width: 50px;
+    height: 10px;
+    margin: auto;
+    background-color: #26ebb4;
+    margin-bottom: 17px;
+  }
+
 `
 
 const Tagline = styled.p`
   margin-top: 0;
   color: white;
+  font-size: 1.2em;
 `
 
 
@@ -30,15 +45,12 @@ const Header = props => (
     <Navbar />
     <Container>
       <div className="grid">
-       <div className="grid__col grid__col--2-of-4">
+       <div className="grid__col grid__col--2-of-4 grid__col--centered">
          <Section>
            <Title>Pokery</Title>
            <Tagline> Sign up to Pokery to find a poker game near you and to enhance your poker playing skills. </Tagline>
-           <Button>Sign Up</Button>
+           <ButtonLink to="/register" pill="true" width="140px">Sign Up</ButtonLink>
          </Section>
-       </div>
-       <div className="grid__col grid__col--2-of-4">
-          {/* <img src="/img/phone.png" alt="phone"/> */}
        </div>
      </div>
     </Container>
