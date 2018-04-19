23cf6fb63857e031306ebd979aad4c42e26b2328
joshuacassidy <Joshcassidy79@gmail.com>
2018-04-19 13:34:09 +0100

    fixed the merge conflicts

--- client/src/shared/PokerSessions/PokerSession/index.js
+++ client/src/shared/PokerSessions/PokerSession/index.js
@@ -3,47 +3,6 @@ import Table from '../../LivePokerGames/LivePokerGame/Table'
 import TableBox from '../../LivePokerGames/LivePokerGame/TableBox'
 import Wrapper from '../../LivePokerGames/LivePokerGame/Wrapper'
 import Header from '../../LivePokerGames/LivePokerGame/Header'
-<<<<<<< HEAD
-const PokerSession = props => (
- <Wrapper>
-   <Header>
-     <h3>{props.variation}</h3>
-     <h4>{props.location}</h4>
-   </Header>
-   {/* <Box>
-     Earnings: {props.session.earnings}
-   </Box> */}
-   <TableBox>
-    <Table>
-      <table>
-        <tbody>
-         <tr>
-          <th>Earnings:</th>
-          <td>{props.amountWon}</td>
-         </tr>
-         <tr>
-          <th>Game Type:</th>
-          <td>{props.gameType}</td>
-         </tr>
-         <tr>
-          <th>Buy In:</th>
-          <td>{props.buyIn}</td>
-         </tr>
-         <tr>
-          <th>Position:</th>
-          <td>Hi</td>
-         </tr>
-         <tr>
-          <th>Note:</th>
-          <td>Hi</td>
-         </tr>
-        </tbody>
-      </table>
-    </Table>
-   </TableBox>
-  </Wrapper>
-)
-=======
 import Button from '../../Button'
 import Input from '../../Input'
 import Select from '../../Select'
@@ -273,6 +232,5 @@ class PokerSession extends React.Component {
     )
   }
 }
->>>>>>> 3bca346b65a403118f33d8170bf0ddffd68e1ede
 
 export default PokerSession
