2480d6b9b98cb6891d3e0e471b4ea5808a9bb445
larfoley <lar_x87@yahoo.com>
2018-04-22 11:55:21 +0100

    updated live poker locations

--- routes/live-poker-timetables.js
+++ routes/live-poker-timetables.js
@@ -1,15 +1,20 @@
 var express = require('express')
 var router = express.Router()
 const rp = require('request-promise')
-const pokerGames = require('./poker-games')
+const casinos = require('../casinos')
 const scrapeLivePoker = require('./scrape-live-poker')
 let tournamentsGames = []
 
 router.get('/', (req, res, next) => {
 
 	const dayToFilterBy = req.query.day
-	// const country = req.query.country
-	const country = 'Ireland'
+	const country = req.query.country
+	let countryIsSupported = !!casinos.find(casino => casino.country === country)
+
+	if (!countryIsSupported) {
+		console.log("country not supported");
+		return res.status(400).json({message: "country not supported"})
+	}
 
 	function getHtml(url) {
 		return rp(url);
@@ -27,22 +32,21 @@ router.get('/', (req, res, next) => {
 		}, Promise.resolve());
 	}
 
-	if (pokerGames.hasOwnProperty(country)) {
+	const casinosURLs = casinos.find(casino => casino.country === country).casinos
 
-		workMyCollection(pokerGames[country])
-		  .then(() => {
-				if (dayToFilterBy) {
-					tournamentsGames = tournamentsGames.filter(game => game.day === dayToFilterBy)
-				}
-				res.json(tournamentsGames)
-			})
-			.catch(err => next(err))
-
-	} else {
-		next(new Error("Your country is not supported"))
-	}
-
-
+	workMyCollection(casinosURLs)
+	  .then(() => {
+			console.log(dayToFilterBy, 'day');
+			if (dayToFilterBy) {
+	
+				tournamentsGames = tournamentsGames.filter(
+					game => game.day.toLowerCase() === dayToFilterBy.toLowerCase()
+				)
+				console.log(tournamentsGames);
+			}
+			res.json(tournamentsGames)
+		})
+		.catch(err => next(err))
 
 
 });
