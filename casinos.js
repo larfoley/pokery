82a225cf46914f4e6a437f6c7971d3edbe74c271
larfoley <lar_x87@yahoo.com>
2018-04-22 11:50:44 +0100

    changed format of casinos files to include casino names

---
+++ casinos.js
@@ -0,0 +1,152 @@
+module.exports = [
+  {
+    country: "Ireland",
+    casinos: [
+      {
+        name: "the eglinton",
+        url: "https://en.clubpoker.net/poker-tournaments/the-eglinton/ireland/0-388-0-0-0-0-0-111"
+      },
+      {
+        name: "penthouse casion & card club",
+        url: "https://en.clubpoker.net/poker-tournaments/penthouse-casino-card-club/ireland/0-391-0-0-0-0-0-111"
+      },
+      {
+        name: "the fitzwilliam casino & card club",
+        url: "https://en.clubpoker.net/poker-tournaments/the-fitzwilliam-casino-card-club/ireland/0-426-0-0-0-0-0-111"
+      },
+      {
+        name: "the sporting emporium",
+        url: "https://en.clubpoker.net/poker-tournaments/the-sporting-emporium/ireland/0-394-0-0-0-0-0-111"
+      },
+      {
+        name: "village green card club",
+        url: "https://en.clubpoker.net/poker-tournaments/village-green-card-club/ireland/0-387-0-0-0-0-0-111"
+      }
+    ]
+  },
+  {
+    country: "France",
+    casinos: [
+      {
+        name: "barriere poker tour deepstack",
+        url: "https://en.clubpoker.net/poker-tournaments/barriere-poker-tour-deepstack/france/106-0-0-0-0-0-0-1"
+      },
+      {
+        name: "barriere poker tour",
+        url: "https://en.clubpoker.net/poker-tournaments/barriere-poker-tour/france/34-0-0-0-0-0-0-1"
+      }
+    ]
+  },
+  {
+    country: "Canada",
+    casinos: [
+      {
+        name: "playground poker club",
+        url: "https://en.clubpoker.net/poker-tournaments/playground-poker-club/toronto/0-349-4-0-0-0-0-52"
+      },
+      {
+        name: "casino montreal",
+        url: "https://en.clubpoker.net/poker-tournaments/casino-montreal/toronto/0-337-4-0-0-0-0-52"
+      }
+    ]
+  },
+  {
+    country: "Netherlands",
+    casinos: [
+      {
+        name: "holland casino amsterdam",
+        url: "https://en.clubpoker.net/poker-tournaments/holland-casino-amsterdam/netherlands/0-422-0-0-0-0-0-170"
+      }
+    ]
+  },
+  {
+    country: "Spain",
+    casinos: [
+      {
+        name: "casino bahia cadiz",
+        url: "https://en.clubpoker.net/poker-tournaments/casino-bahia-cadiz/spain/0-436-0-0-0-0-0-6"
+      },
+      {
+        name: "penthouse casino card club",
+        url: "https://en.clubpoker.net/poker-tournaments/penthouse-casino-card-club/ireland/0-391-0-0-0-0-https://en.clubpoker.net/poker-tournaments/casino-barcelona/spain/0-327-0-0-0-0-0-6"
+      },
+      {
+        name: "casino gran madrid",
+        url: "https://en.clubpoker.net/poker-tournaments/casino-gran-madrid/spain/0-429-0-0-0-0-0-6"
+      }
+    ]
+  },
+  {
+    country: "Switzerland",
+    casinos: [
+      {
+        name: "grand casino baden",
+        url: "https://en.clubpoker.net/poker-tournaments/grand-casino-baden/switzerland/0-333-0-0-0-0-0-4"
+      },
+      {
+        name: "swiss casinos zurich",
+        url: "https://en.clubpoker.net/poker-tournaments/swiss-casinos-zurich/switzerland/0-332-0-0-0-0-0-4"
+      }
+    ]
+  },
+  {
+    country: "China",
+    casinos: [
+      {
+        name: "pokerstars live macau",
+        url: "https://en.clubpoker.net/poker-tournaments/pokerstars-live-macau/macao/0-404-0-0-0-0-0-154"
+      }
+    ]
+  },{
+    country: "Austrailia",
+    casinos: [
+      {
+        name: "crown casino",
+        url: "https://en.clubpoker.net/poker-tournaments/crown-casino/east/0-418-3-0-0-0-0-29"
+      }
+    ]
+  },
+  {
+    country: "Belgium",
+    casinos: [
+      {
+        name: "casino spa",
+        url: "https://en.clubpoker.net/poker-tournaments/casino-spa/belgium/0-232-0-0-0-0-0-2"
+      },
+      {
+        name: "grand casino namur",
+        url: "https://en.clubpoker.net/poker-tournaments/grand-casino-namur/belgium/0-231-0-0-0-0-0-2"
+      }
+    ]
+  },
+  {
+    country: "England",
+    casinos: [
+      {
+        name: "aspers casino",
+        url: "https://en.clubpoker.net/poker-tournaments/aspers-casino/england/0-363-1-0-0-0-0-89"
+      },
+      {
+        name: "the casino at empire",
+        url: "https://en.clubpoker.net/poker-tournaments/the-casino-at-empire/england/0-365-1-0-0-0-0-89"
+      },
+      {
+        name: "river card club",
+        url: "https://en.clubpoker.net/poker-tournaments/river-card-club/england/0-370-1-0-0-0-0-89"
+      }
+    ]
+  },
+  {
+    country: "Scotland",
+    casinos: [
+      {
+        name: "genting casino glasgow",
+        url: "https://en.clubpoker.net/poker-tournaments/genting-casino-glasgow/scotland/0-402-3-0-0-0-0-89"
+      },
+      {
+        name: "genting club edinburgh",
+        url: "https://en.clubpoker.net/poker-tournaments/genting-club-edinburgh/scotland/0-401-3-0-0-0-0-89"
+      }
+    ]
+  }
+]
